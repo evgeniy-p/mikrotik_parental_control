58c213acb056984c4bbe53257972da00d95a2e73
evgeniy-p <mariwield@gmail.com>
2017-07-18 15:28:39 +0300

    update

---
+++ sched_but.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled3.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(384, 134)
+        self.dateTimeEdit = QtWidgets.QDateTimeEdit(Form)
+        self.dateTimeEdit.setGeometry(QtCore.QRect(30, 20, 194, 27))
+        self.dateTimeEdit.setObjectName("dateTimeEdit")
+        self.dateTimeEdit_2 = QtWidgets.QDateTimeEdit(Form)
+        self.dateTimeEdit_2.setGeometry(QtCore.QRect(30, 60, 194, 27))
+        self.dateTimeEdit_2.setObjectName("dateTimeEdit_2")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(250, 20, 99, 27))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(250, 60, 99, 27))
+        self.pushButton_2.setObjectName("pushButton_2")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "PushButton"))
+        self.pushButton_2.setText(_translate("Form", "PushButton"))
+
